779fab52e5f4fe3462b8989fc773324fecff8d90
Comp <buca-rock@mail.ru>
2022-07-12 16:04:16 +0300

    DZ_2_Evgeny_2021

--- Evgeny_2021/project/urls.py
+++ Evgeny_2021/project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.),
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
